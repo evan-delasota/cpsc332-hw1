339fd15cbe40f1e8eb1da65bc20f8e382c394d8c
Evan D <evan.delasota@gmail.com>
2021-03-16 15:43:53 -0700

    Added BFS implementation, edited query in departmentstats

--- DepartmentStats.ps1
+++ DepartmentStats.ps1
@@ -3,16 +3,67 @@
 # Invoke-SQLCmd -HostName "localhost" -Database "CPSC332" -InputFile ".\00. Company.sql" -ErrorAction Stop;
 
 $OutputFile = ".\DepartmentStats.csv";
-$SqlQuery = "SELECT Dname AS DepartmentName, SUM(DISTINCT EMPLOYEE.Salary) AS SumOfSalary,
-AVG(DISTINCT Salary) AS AvgOfSalary, SUM(ISNULL(WORKS_ON.Hours, 0)) AS SumOfWorkingHours, 
-COUNT(DISTINCT Pno) AS TotalNumOfProjects
+$SqlQuery = "
+SELECT FirstCol.DepartmentName,
+    SumOfSalary,
+    AvgOfSalary,
+    SumOfWorkingHours,
+    TotalNumOfProjects
+FROM
+(
+    SELECT DEPARTMENT.Dname AS DepartmentName, TotalNumOfProjects FROM
+    (
+        SELECT PROJECT.Dnum,
+            Count(DISTINCT WORKS_ON.Pno) AS TotalNumOfProjects
+            FROM   WORKS_ON
+            INNER JOIN PROJECT 
+            ON PROJECT.Pnumber = WORKS_ON.Pno
+            GROUP  BY PROJECT.Dnum
+    ) AS p
+    INNER JOIN DEPARTMENT    
+    ON DEPARTMENT.Dnumber = p.Dnum
+) AS FirstCol
+    
+INNER JOIN 
+(
+    SELECT SecondCol.DepartmentName,
+    ThirdCol.SumOfSalary,
+    ThirdCol.AvgOfSalary,
+    SecondCol.SumOfWorkingHours
+    FROM
+    (
+        SELECT FirstQuery.Dname  AS DepartmentName,
+        Sum(SecondQuery.HOURS) AS SumOfWorkingHours
+        FROM DEPARTMENT AS FirstQuery
+        FULL JOIN
+        (
+            SELECT * FROM   WORKS_ON
+            FULL JOIN PROJECT 
+            ON WORKS_ON.Pno = PROJECT.Pnumber
+        ) AS SecondQuery 
+        ON firstQuery.Dnumber = SecondQuery.Dnum
+        GROUP  BY FirstQuery.Dname
+    ) AS SecondCol
+    
+    INNER JOIN 
+    (
+        SELECT * FROM
+        (
+            SELECT (d.Dname) AS DepartmentName,
+            Sum(e.salary) AS SumOfSalary,
+            Avg(e.salary) AS AvgOfSalary
+            FROM employee AS e
+                    RIGHT OUTER JOIN
+                    DEPARTMENT AS d
+                                ON
+                    d.Dnumber = e.Dno
+            GROUP  BY d.Dname
+        ) AS e
+    ) AS ThirdCol 
+    ON SecondCol.DepartmentName = ThirdCol.DepartmentName
 
-FROM EMPLOYEE
-INNER JOIN WORKS_ON
-ON EMPLOYEE.Ssn = WORKS_ON.Essn
-INNER JOIN DEPARTMENT
-ON Employee.Dno = DEPARTMENT.Dnumber
-GROUP BY DEPARTMENT.Dname;";
+) AS FourthCol
+ON FirstCol.DepartmentName = FourthCol.DepartmentName";
 
 $SqlConnection = New-Object System.Data.SqlClient.SqlConnection;
 $SqlConnection.ConnectionString = "Server = ETD; Database = CPSC332; Integrated Security = True";
@@ -20,13 +71,9 @@ $SqlConnection.Open();
 
 $SqlCmd = New-Object System.Data.SqlClient.SqlCommand;
 $SqlCmd.Connection = $SqlConnection;
-
 $SqlCmd.CommandText = $SqlQuery;
 
-# while ($Data.Read()) {
-#     Write-Host $Data["DepartmentName", "SumOfSalary", "AvgOfSalary", "SumOfWorkingHours", "TotalNumOfProjects"];
-# };
-
+$SqlConnection.Close();
 $SqlAdapter = New-Object System.Data.SqlClient.SqlDataAdapter;
 $SqlAdapter.SelectCommand = $SqlCmd;
 
